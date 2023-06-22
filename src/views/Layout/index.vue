e01358675a12c93c17a4bb155c209b8889ee8c10
1350171526 <1350171526@qq.com>
2023-06-22 16:50:32 +0800

    Layout静态结构搭建

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,8 +1,11 @@
+<script setup>
+import LayoutNav from './components/LayoutNav.vue'
+import LayoutHeader from './components/LayoutHeader.vue'
+import LayoutFooter from './components/LayoutFooter.vue'
+</script>
 <template>
-    <div>
-        首页
-        <!-- 设置二级路由出口 -->
-        <RouterView />
-    </div>
-    
+    <LayoutNav />
+    <LayoutHeader />
+    <RouterView />
+    <LayoutFooter />
 </template>
