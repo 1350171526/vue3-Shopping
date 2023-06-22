adaa2d9efaaa7afaa2fe5072306d2356a4370c68
1350171526 <1350171526@qq.com>
2023-06-22 17:26:04 +0800

    渲染一级导航列表

--- src/utils/http.js
+++ src/utils/http.js
@@ -13,6 +13,6 @@ httpInstance.interceptors.request.use(config => {
 
 // axios响应拦截器
 httpInstance.interceptors.response.use(res => res.data, e=> {
-    return Promise.reject(e) 
+    return Promise.reject(e)
 })
 export default httpInstance
