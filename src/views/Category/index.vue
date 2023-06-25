486a1cd1d48154330a7434184918b473a7bd2403
1350171526 <1350171526@qq.com>
2023-06-25 17:48:55 +0800

    路由缓存问题解决

--- src/views/Category/index.vue
+++ src/views/Category/index.vue
@@ -5,15 +5,23 @@ import { onMounted, ref } from 'vue';
 import { useRoute } from 'vue-router';
 import { getBannerAPI } from '@/apis/home'
 import GoodsItem from '@/views/Home/components/GoodsItem.vue'
+// 解决路由复用问题
+import { onBeforeRouteUpdate } from 'vue-router';
+import router from '@/router';
 
 // 获取数据
 const categoryData = ref({})
 const route = useRoute()
-const getCategory = async () => {
-    const res = await getCategoryAPI(route.params.id)
+const getCategory = async (id = router.params.id) => {
+    const res = await getCategoryAPI(id)
     categoryData.value = res.result
 }
 onMounted(() => getCategory())
+
+// 路由参数变化的时候 可以把分类数据接口重新发送
+onBeforeRouteUpdate((to) => {
+    getCategory(to.params.id)
+})
 
 // banner
 const bannerList = ref([])
