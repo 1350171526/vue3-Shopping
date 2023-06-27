30ea573409302f461d8614283700e88fba193748
1350171526 <1350171526@qq.com>
2023-06-27 12:29:05 +0800

    详情热榜-基础组件封装和基础数据获取渲染

--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -1,4 +1,6 @@
 <script setup>
+
+import DetailHot from './components/DetailHot.vue'
 import { getDetail } from '@/apis/detail'
 import { onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
@@ -123,6 +125,10 @@ onMounted(() => getGoods())
             </div>
             <!-- 24热榜+专题推荐 -->
             <div class="goods-aside">
+              <!-- 24小时 -->
+              <DetailHot />
+              <!-- 周 -->
+              <DetailHot />
 
             </div>
           </div>
