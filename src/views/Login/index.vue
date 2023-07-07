d6678ef2ca9100f2110dc57dc4001fba98383498
1350171526 <1350171526@qq.com>
2023-07-07 11:26:20 +0800

    登录-表单校验-统一校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -24,6 +24,15 @@ const rules = {
     }
   ]
 }
+
+const fromRef = ref(null)
+const doLogin = () =>{
+  fromRef.value.validate((valid) => {
+    if (valid){
+
+    }
+  })
+}
 </script>
 
 
@@ -48,7 +57,7 @@ const rules = {
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form :model="userInfo" :rules="rules" label-position="right" label-width="60px"
+            <el-form ref="formRef" :model="userInfo" :rules="rules" label-position="right" label-width="60px"
               status-icon>
               <el-form-item prop="account" label="账户">
                 <el-input v-model="userInfo.account" />
@@ -61,7 +70,7 @@ const rules = {
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
-              <el-button size="large" class="subBtn">点击登录</el-button>
+              <el-button size="large" class="subBtn" @click="doLogin">点击登录</el-button>
             </el-form>
           </div>
         </div>
